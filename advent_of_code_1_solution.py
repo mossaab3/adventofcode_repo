b28649e4c1d0eed0434e2bd2cda6a758f743157c
Mosaab <mosaab.derbal@heracles-robotics.com>
2025-04-22 22:27:11 +0200

    advent_of_code_exercise_1 solution

---
+++ advent_of_code_1_solution.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+### ---- Extracting data from the csv file
+data = np.loadtxt("advent_1_data.csv")  
+data_1 = data[:, 0]  # first column
+data_2 = data[:, 1]  # second column
+
+### ---- Keeping it lists so that it is the most general case altough numpy is faster 
+data_1 = list(data_1)
+data_2 = list(data_2)
+
+
+### ---- This function calculates the total distance between a list of tuples
+def calculate_total_difference(list_of_tuples) -> float:
+    total_distance = 0
+    for i in range(len(list_of_tuples)):
+        total_distance += abs(list_of_tuples[i][0] - list_of_tuples[i][1])
+    
+    print("The total distance is:", total_distance)
+    return total_distance
+
+### ---- This function matches two lists of the same size and returns a list of tuples
+def match_lists(input_1, input_2) -> list:
+
+    ## asserting input_1 and input_2 are of the same size to be matched and their size is bigger than 0
+    assert(len(input_1) > 0) and len(input_2), "Input lists must be of size greater than 0."
+    assert len(input_1) == len(input_2), "Input lists must be of the same length."
+
+    ## here we create a list of tuples with the values of the two lists
+    size = len(input_1)
+
+    matched_list_of_tuples = []
+    for i in range(size):
+        minimum_input_1 = min(input_1)
+        minimum_input_2 = min(input_2)
+        index_input_1 = input_1.index(minimum_input_1)
+        index_input_2 = input_2.index(minimum_input_2)
+        matched_list_of_tuples.append((minimum_input_1, minimum_input_2))
+        input_1.pop(index_input_1)
+        input_2.pop(index_input_2)
+    
+    print("Input lists are matched and the result is:", matched_list_of_tuples)
+    return matched_list_of_tuples
+
+### ---- Test for advent_1_data.csv
+if __name__=="__main__":
+    matched_list = match_lists(data_1, data_2)
+    calculate_total_difference(matched_list)
